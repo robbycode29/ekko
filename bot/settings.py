d75c2e068eb4ff6087e7b69dbff79d41eff3df7c
Robert Oprescu <robert.oprescu_ext@sonepar.com>
2024-11-19 13:01:21 +0200

    feature/SRI-12 adding env var for cohere

--- bot/settings.py
+++ bot/settings.py
@@ -10,10 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Access the environment variables
+COHERE_TOKEN = os.getenv('COHERE_TOKEN')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
